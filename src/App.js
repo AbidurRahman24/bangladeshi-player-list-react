d6ba292830133b56dcd3e78e3edae7df100ecc3b
Niloy-Sumon <niloyabidur@gmail.com>
2021-07-08 07:46:43 +0600

    player

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  // const [player, setPlayer] = useState([])
+  // useEffect(
+  //   fetch('https://mocki.io/v1/f4f44a79-11d2-4654-81fa-45ccf03563f8')
+  // .then(res => res.json())
+  // .then(data => setPlayer(data))
+  //   ,[])
+  return (
+    <div>
+      <h2>Hello</h2>
+    </div>
+  );
+}
+
+export default App;
